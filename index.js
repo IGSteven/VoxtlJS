c4a71805b574990aa094dbe539f8822510dca06c
Steven Smith <me@igsteven.com>
2020-10-29 19:07:56 +0000

    1.0.2 Snapshot
    This is not a complete version, please wait for commit '1.0.2 Update'
    
    
    Version: 1.0.2
     - Replaced SocketIO with WS client to reflect a change in Voxtl/Nebula
     - Updated Login Function (We now no longer require the username to be passed in the login function after the following change to Voxtl/Nebula https://github.com/Voxtl/Nebula/compare/ddcce0386f...ff1e8fc232
    - Tidied up the class a little

--- index.js
+++ index.js
@@ -1,122 +1,107 @@
-"use strict";
+'use strict';
 
 // Requirements
-const getJSON = require('get-json');
-const socket = require('socket.io-client')
+const WebSocket = require('ws');
 const EventEmitter = require('events');
 
 // Initializing a constructor function
 class VoxtlJS extends EventEmitter {
 
-    constructor(channel) {
-        if (channel === undefined) return console.log('VoxtlJS: No voxtl channel has been defined.');
+    // Object Structure
+    constructor(definedSettings) {
         super(); // Event Emitter
-        this.io = new socket('https://chat.voxtl.com'); // Make a new Socket when New is run on VoxtlJS
-        this.token = undefined;
-        this.bot = {
-            username: 'Steve',
-            token: this.token
+
+        var Settings = {
+            Sessions: 1,
+            Watchonly: false,
+            ratelimit: 100, // This will want to be updated to the Voxtl Default.
+            debug: false
         }
 
-        // Channel Data (we update this later just a inital placeholder)
-        this.channel = {
-            name: channel,
-            followers: 0,
-            viewers: 0,
-            stream: {
-                title: 'Loading Title.',
-                catagory: 'Loading Catagory.'
+        this.bot = {
+            username: null,
+            login: {
+                status: false,
+                token: null,
+                TimeSinceLogin: null,
             }
         }
 
-        function interval() {
-            return new Promise(function (resolve, reject) {
-                setInterval(function () {
-                    getJSON(`https://api.voxtl.com/comet/stream/info?channel=${channel}`)
-                        .then(function (data) {
-                            channel = {
-                                name: channel,
-                                followers: data.result.followers,
-                                viewers: data.result.viewers,
-                                stream: {
-                                    title: data.result.stream.title,
-                                    catagory: data.result.stream.category
-                                }
-                            }
-                            return resolve(channel);
-                        }).catch(function (error) {
-                            return reject(error);
-                        });
-                }, 25000)
-            })
-        };
-        interval().then((data) => {
-            this.channel = data;
+        this.socket = []; // Creates a array for chat socket sessions (session per channel currently) 
+        this.channels = []; // Creates a array for channels
+        this.users = []; // Creates a array for users
+    }
+
+    // Functions
+
+    // Login to Voxtl with Token
+    login = function (token) {
+        // This is used to valid and store the token for the bot.
+        // This script makes a connection to the chat for the VoxtlJS user and waits for the Ready event to confirm who we are logged in as.
+
+        // If no token is provide this will throw a error
+        if (!token) throw "VoxtlJS: No Token was provided.";
+        
+        // Creates a new socket for the connection to valid the token // Conection uses the VoxtlJS UUID as currently channel names don't work
+        let TokenValidationSocket = new WebSocket('wss://ws.voxtl.tv/7046d794-72f2-4366-970b-4142daaca79a')
+        // Once this connection is started, we now tell the server we are logging in.
+        TokenValidationSocket.on('open', function open() {
+            var loginstring = {
+                "event": "join",
+                "data": token
+            }
+            // Send data to websocket (chat server)
+            TokenValidationSocket.send(JSON.stringify(loginstring))            
+        })
+
+        // On Message event, waiting for type ready.
+        TokenValidationSocket.on('message', (data) => {
+            data = JSON.parse(data);
+            if (data.event === 'ready'){
+                this.bot.id = data.data.id
+                this.bot.username = data.data.username;
+                this.bot.login.token = token
+                this.bot.login.status = true;
+                this.bot.login.TimeSinceLogin = Date();
+            }
+            console.log(data.data);
         });
     }
 
-    // END OF CONSTRUCTOR
-
-    // Enable Debug Mode
-    debug = function (Bool) {
-        if (Bool == true) {
-            console.log('VoxtlJS: Debugger now enabled!');
-            var debugListeners = [
-                this.io.on('connect', data => { console.log('VoxtlJS: Connected to Voxtl chat server!') }),
-                this.io.on('event', data => { console.log('VoxtlJS: '.data) }),
-                this.io.on('chatMessage', data => { console.log('VoxtlJS: '.data) }),
-                this.io.on('disconnect', data => { console.log('VoxtlJS: Disconnected from Voxtl chat server!') })
-            ]
+    debug = function (Bool) { // Toggle debug (Function can be turned on and off within the application)
+        if (Bool == true)
+        {
+            return 'VoxtlJS: Debugger now enabled!';
         }
-        else if (Bool == false) {
-            console.log('VoxtlJS: Debugger now disabled!');
-            if (debugListeners == undefined) return;
-            debugListeners.forEach(Listener => {
-                this.removeListener('connection', Listener);
-            });
+        else if (Bool == false)
+        {
+            return 'VoxtlJS: Debugger now disabled!';
         }
-        else return console.log('VoxtlJS: Debug setting ether must be true or false')
+        else
+        {
+            throw 'VoxtlJS.debugging was called without \'True\' or \'False\' being defined.'
+        }
     }
 
-    // Login with Token
-    login = function (username, token) {
-        this.bot.username = username;
-        this.token = token;
-
-        //if (!token) return console.log("VoxtlJS: No Token was provided.")
-        this.io.on('connect', client => {
-            this.io.emit('joinChannel', {
-                'type': 'connect',
-                'time': Math.floor(Date.now() / 1000),
-                'token': this.token,
-                'data': {
-                    'username': this.bot.username,
-                    'channel': this.channel.name
-                }
-            })
-
-            // Client Events
-            // Chat Message
-            this.io.on('chatMessage', data => { this.emit('chatMessage', data) })
-        })
-        this.emit('ready', { 'state': 'ready', 'channel': this.channel })
-    }
-
-    // Send Message
-    sendMessage = function (msg) {
-        // if (this.token === undefined) return console.log('VoxtlJS: you must login to send chat messages')
-        let messageData = {
-            'type': 'message',
-            'time': Math.floor(Date.now() / 1000),
-            'token': this.token,
-            'data': {
-                'username': this.bot.username,
-                'message': msg
-            }
-        }
-        this.io.emit('chatMessage', messageData)
-    }
 }
+
+
+class user {
+}
+class channel {
+}
+
+
+function CodeGen(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+ }
+ 
 
 // Module Export
 module.exports = VoxtlJS;
